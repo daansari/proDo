0e8c347fddf1a5ad9c95567dbb17aaaad6c0a6c3
Danish Ahmed Ansari <mail.danishaa@gmail.com>
2015-04-18 15:39:46 +0530

    Added text partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
 //= require_tree .
+
+$(function(){
+  /* Your JavaScript goes here... */
+});

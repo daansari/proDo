2d3a9d01c763f588aafbc72c11076fe9c1088f09
Danish Ahmed Ansari <mail.danishaa@gmail.com>
2015-04-29 17:52:48 +0530

    Welcome Page added to Root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  
+  get 'landing_page' => 'landing_page#welcome'
 
+  root 'landing_page#welcome'
+
+# root landing_page
   # You can have the root of your site routed with "root"
   get 'prodo' =>  'pages#home'
 

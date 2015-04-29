e4bf017d6df641edb46bbcaf76bdbd39f5b689fd
Danish Ahmed Ansari <mail.danishaa@gmail.com>
2015-04-29 17:37:52 +0530

    Added prod path in navigation brand

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  get 'home' =>  'pages#home'
+  get 'prodo' =>  'pages#home'
 
   get 'about' => 'pages#about'
 

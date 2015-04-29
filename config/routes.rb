23bfeea7be040896f778c7bd337c22c7e6155f79
Danish Ahmed Ansari <mail.danishaa@gmail.com>
2015-04-29 17:04:33 +0530

    Routes Changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+  get 'home' =>  'pages#home'
 
   get 'about' => 'pages#about'
 
